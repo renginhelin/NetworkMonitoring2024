46e7130ca98570bd2d80522be1220ea6a25b41cb
Monarch34 <omerzembat@hotmail.com>
2024-07-29 10:43:32 +0300

    arp table eklendi ve proje dosyası güncellendi

--- netmiko_project/netmiko_2.py
+++ netmiko_project/netmiko_2.py
@@ -2,68 +2,110 @@ import netmiko
 import threading
 import tempfile
 import os
+import time
 
 
-def send_ping_to_multiple_devices(connection, devices: list):
-    result = []
-    try:
-        for device in devices:
-            output = connection.send_command(f"ping {device['host']}")
-            result.append(output)
-        return result
-    except (netmiko.exceptions.SSHException, netmiko.exceptions.ConnectionException):
-        print("Connection failed")
-    except netmiko.exceptions.NetMikoTimeoutException:
-        print("Timeout")
+class NetmikoHandler:
+    def __init__(self, host):
+        self.host = host
+        self.connection = None
+
+    def connect(self):
+        try:
+            self.connection = netmiko.ConnectHandler(**self.host)
+        except Exception as e:
+            print(f"Connection failed: {e}")
+
+    def disconnect(self):
+        if self.connection:
+            self.connection.disconnect()
+
+    def send_ping_to_multiple_devices(self, devices: list):
+        result = []
+        try:
+            for device in devices:
+                output = self.connection.send_command(f"ping {device['host']}")
+                result.append(output)
+            return result
+        except (netmiko.exceptions.SSHException, netmiko.exceptions.ConnectionException):
+            print("Connection failed")
+        except netmiko.exceptions.NetMikoTimeoutException:
+            print("Timeout")
+
+    def show_interface_brief(self):
+        try:
+            output = self.connection.send_command("show ip interface brief")
+            return output
+        except netmiko.exceptions.NetMikoTimeoutException:
+            print("Timeout")
+
+    def show_inventory(self):
+        try:
+            output = self.connection.send_command("show inventory")
+            return output
+        except netmiko.exceptions.NetMikoTimeoutException:
+            print("Timeout")
+
+    def show_hardware_and_version(self):
+        index_for_extract = [0, 3, 5, 6, 8, 28, 29, 30, 31, 32, 33, 35]
+        try:
+            output = self.connection.send_command("show hardware").split("\n")
+            output = "\n".join([output[index] for index in index_for_extract])
+            return output
+        except netmiko.exceptions.NetMikoTimeoutException:
+            print("Timeout")
+
+    def show_arp_table(self):
+        try:
+            output = self.connection.send_command("show arp")
+            return output
+        except netmiko.exceptions.NetMikoTimeoutException:
+            print("Error on show arp table")
 
 
-def show_interface_brief(connection):
-    try:
-        output = connection.send_command("show ip interface brief")
-        return output
-    except netmiko.exceptions.NetMikoTimeoutException:
-        print("Timeout")
+def thread_function(host:dict, devices: list, tmp_filename):
+    total_time = 0
+    handler = NetmikoHandler(host)
+    start_time = time.time()
+    handler.connect()
+    end_time = time.time()
+    total_time += end_time - start_time
+    print(f"1-host {host["host"]}   {start_time - end_time}")
 
+    if handler.connection:
+        with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.txt') as tmp:
+            start_time = time.time()
+            ping_output_list = handler.send_ping_to_multiple_devices(devices)
+            end_time = time.time()
+            total_time += end_time - start_time
+            print(f"2-host {host["host"]}  {start_time - end_time}")
 
-def show_inventory(connection):
-    try:
-        output = connection.send_command("show inventory")
-        return output
-    except netmiko.exceptions.NetMikoTimeoutException:
-        print("Timeout")
+            start_time = time.time()
+            interface_output_list = handler.show_interface_brief()
+            hardware_and_version_list = handler.show_hardware_and_version()
+            inventory_list = handler.show_inventory()
+            arp_table = handler.show_arp_table()
+            end_time = time.time()
+            total_time += end_time - start_time
+            print(f"3-host {host["host"]}   {start_time - end_time}")
+            print(f"Total time: {total_time}")
+            tmp.write("-" * 70 + "\n")
+            tmp.write("\n".join(ping_output_list) + '\n')
+            tmp.write("\n" + interface_output_list + "\n")
+            tmp.write("\n" + hardware_and_version_list + "\n")
+            tmp.write("\n" + inventory_list)
+            tmp.write("\n" + arp_table)
+            tmp.write("\n")
 
+            tmp.flush()
+            handler.disconnect()
 
-def hardware_and_version(connection):
-    index_for_extract = [0, 3, 5, 6, 8, 28, 29, 30, 31, 32, 33, 35]
-    try:
-        output = connection.send_command("show hardware").split("\n")
-        output = "\n".join([output[index] for index in index_for_extract])
-        return output
-    except netmiko.exceptions.NetMikoTimeoutException:
-        print("Timeout")
-
-
-def thread_function(host, devices: list, tmp_filename):
-    with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.txt') as tmp:
-        connection = netmiko.ConnectHandler(**host)
-        ping_output_list = send_ping_to_multiple_devices(connection, devices)
-        interface_output_list = show_interface_brief(connection)
-        hardware_and_version_list = hardware_and_version(connection)
-        show_inventory_list = show_inventory(connection)
-
-        tmp.write("-" * 70 + "\n")
-        tmp.write("\n".join(ping_output_list) + '\n')
-        tmp.write("\n" + interface_output_list + "\n")
-        tmp.write("\n" + hardware_and_version_list + "\n")
-        tmp.write("\n" + show_inventory_list)
-        tmp.flush()
-        connection.disconnect()
-
-        # Store the name of the temporary file
-        tmp_filename.append(tmp.name)
+            # Store the name of the temporary file
+            tmp_filename.append(tmp.name)
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     try:
         host1 = {
             "device_type": 'cisco_ios',
@@ -84,7 +126,6 @@ if __name__ == "__main__":
             "password": 'cisco123',
         }
         devices = [host1, host2, host3]
-
         tmp_filenames = []
 
         t1 = threading.Thread(target=thread_function, args=(host1, devices, tmp_filenames))
@@ -106,6 +147,8 @@ if __name__ == "__main__":
                     output_file.write(tmp_file.read())
                 # Optionally, delete the temporary file after reading
                 os.remove(tmp_filename)
-
+        end_time = time.time()
+        print(f"execute: {end_time - start_time}")
+        print("Successful")
     except Exception as e:
         print(f"An error occurred: {e}")
